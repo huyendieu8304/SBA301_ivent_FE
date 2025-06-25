19c4d1b1423492b4fed9de98b740a18779647d34
Bui Minh Chien <chien181004@gmail.com>
2025-06-25 08:34:33 +0700

    add table component

---
+++ src/component/TableComponent.jsx
@@ -0,0 +1,76 @@
+import { DataGrid } from "@mui/x-data-grid";
+import React, { useEffect, useRef } from "react";
+import renderCellExpand from "./GridCellExpand.jsx";
+
+//css for table
+const dataGridStyle = {
+    "& .MuiDataGrid-columnHeaderTitle": {
+        whiteSpace: "normal",
+        lineHeight: "normal",
+    },
+    "& .MuiDataGrid-columnHeaderTitleContainer": {
+        display: "flex !important",
+        flexDirection: "row !important",
+        alignItems: "center !important",
+    },
+    "& .MuiBox-root": {
+        height: "100%",
+        alignContent: "center",
+    },
+};
+const baseColumnOptions = {
+    sortable: false,
+    filterable: false,
+    pinnable: false,
+    hideable: false,
+};
+const translateColumn = (listColumn) => {
+    return listColumn.map((column) => {
+        return {
+            ...column,
+            ...baseColumnOptions,
+            renderCell: column.renderCell || renderCellExpand,
+        };
+    });
+};
+const CommonTable = ({
+                         columns,
+                         data,
+                         handleClickRow,
+                         pageSizeOptions = [10, 20, 35],
+                         hideFooter = false,
+                         showCellVerticalBorder = false,
+                         showColumnVerticalBorder = false,
+                     }) => {
+    return (
+        <DataGrid
+            hideFooter={hideFooter}
+            showCellVerticalBorder={showCellVerticalBorder}
+            showColumnVerticalBorder={showColumnVerticalBorder}
+            disableColumnFilter
+            disableRowSelectionOnClick
+            disableColumnMenu={true}
+            unstable_rowSpanning={false}
+            columns={translateColumn(columns)}
+            onRowClick={(params, event, details) =>
+                handleClickRow(params, event, details)
+            }
+            rows={data}
+            getRowId={Math.random}
+            slotProps={{
+                pagination: { labelRowsPerPage: "ページあたりの行数" },
+            }}
+            initialState={{
+                ...data.initialState,
+                pagination: {
+                    paginationModel: { pageSize: pageSizeOptions[0] },
+                },
+            }}
+            pageSizeOptions={pageSizeOptions}
+            sx={{
+                ...dataGridStyle,
+            }}
+        />
+    );
+};
+export default CommonTable;
